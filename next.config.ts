091f685a5c14918154a725c1341d04b19011957d
Varushka Sachenko <varvara.sachenko@gmail.com>
2025-11-23 18:44:22 +0300

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  output: 'export', // Включает статический экспорт
+  trailingSlash: true,
+  images: {
+    unoptimized: true, // Для корректного отображения картинок на GitHub Pages
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/IneuroLab-Website' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/IneuroLab-Website/' : '',
+}
 
-export default nextConfig;
+export default nextConfig
